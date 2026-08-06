be9812b348f17f2c3f392030109f8c7b1319acb2
inthewaves <inthewaves@pm.me>
2026-08-05 17:42:57 -0700

    gmscompat: allow GmsCompat to read Bluetooth auto-off timeout
    Allow GmsCompat to inform users if Bluetooth auto-off is on, since it might affect Find Hub
    functionality that they might want.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14470,7 +14470,7 @@ public final class Settings {
         public static final String WIFI_AUTO_OFF = "wifi_off_timeout";
 
         /** @hide */
-        @Protected(readWrite = KnownSystemPackage.SETTINGS)
+        @Protected(read = KnownSystemPackage.GMS_COMPAT, readWrite = KnownSystemPackage.SETTINGS)
         public static final String BLUETOOTH_AUTO_OFF = "bluetooth_off_timeout";
 
         /** @hide */
